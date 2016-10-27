8e163d8d153ac75e4f89ce3897116c0b797853f1
Czuaphe <czuaphe@hotmail.com>
2016-10-27 16:20:01 +0800

    Start Zhihu Project

---
+++ app/src/main/java/com/czuaphe/a10_zhihu/ZhihuApplication.java
@@ -0,0 +1,16 @@
+package com.czuaphe.a10_zhihu;
+
+import android.app.Application;
+
+/**
+ * Created by Czuaphe on 2016/10/27.
+ */
+
+public class ZhihuApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
